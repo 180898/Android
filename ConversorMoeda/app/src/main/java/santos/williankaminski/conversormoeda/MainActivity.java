0a395639f59caef48f13f59cf789816d51f46b12
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2018-11-04 23:23:21 -0200

    Criando o mapeamento dos elementos com a classe ViewHolder

--- ConversorMoeda/app/src/main/java/santos/williankaminski/conversormoeda/MainActivity.java
+++ ConversorMoeda/app/src/main/java/santos/williankaminski/conversormoeda/MainActivity.java
@@ -1,13 +1,32 @@
 package santos.williankaminski.conversormoeda;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.editValue = (EditText) findViewById(R.id.edit_value);
+        this.mViewHolder.textDollar = (TextView) findViewById(R.id.text_dollar);
+        this.mViewHolder.textEuro = (TextView) findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = (Button) findViewById(R.id.button_calculate);
+    }
+
+    private static class ViewHolder{
+        EditText editValue;
+        TextView textDollar;
+        TextView textEuro;
+        Button buttonCalculate;
     }
 }
