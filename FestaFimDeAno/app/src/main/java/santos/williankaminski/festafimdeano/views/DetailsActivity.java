33f80324918e545150c1f2e358214f92fafa1a0d
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2018-11-06 21:41:33 -0200

    Passando valores entre Activitys

--- FestaFimDeAno/app/src/main/java/santos/williankaminski/festafimdeano/views/DetailsActivity.java
+++ FestaFimDeAno/app/src/main/java/santos/williankaminski/festafimdeano/views/DetailsActivity.java
@@ -1,14 +1,15 @@
-package santos.williankaminski.festafimdeano;
+package santos.williankaminski.festafimdeano.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 
+import santos.williankaminski.festafimdeano.R;
 import santos.williankaminski.festafimdeano.constants.FimDeAnoConstants;
 import santos.williankaminski.festafimdeano.util.SecurityPreferences;
 
-public class DetailsActivity extends AppCompatActivity implements View.OnClickListener{
+public class DetailsActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewHolder mViewHolder = new ViewHolder();
     private SecurityPreferences mSecurityPreferences;
@@ -23,21 +24,35 @@ public class DetailsActivity extends AppCompatActivity implements View.OnClickLi
         this.mViewHolder.checkParticipate = (CheckBox) findViewById(R.id.participate);
 
         this.mViewHolder.checkParticipate.setOnClickListener(this);
+
+        this.loadDataFromActivity();
     }
 
     @Override
     public void onClick(View v) {
         int id = v.getId();
-        if(id == R.id.participate){
-            if(this.mViewHolder.checkParticipate.isChecked()){
+        if (id == R.id.participate) {
+            if (this.mViewHolder.checkParticipate.isChecked()) {
                 this.mSecurityPreferences.storageString(FimDeAnoConstants.preferences, FimDeAnoConstants.CONFIRMAED_WILL_GO);
-            }else{
+            } else {
                 this.mSecurityPreferences.storageString(FimDeAnoConstants.preferences, FimDeAnoConstants.CONFIRMAED_WONT_GO);
             }
         }
     }
 
-    private static class ViewHolder{
+    private void loadDataFromActivity() {
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String presence = extras.getString(FimDeAnoConstants.preferences);
+            if (presence == FimDeAnoConstants.CONFIRMAED_WILL_GO) {
+                this.mViewHolder.checkParticipate.setChecked(true);
+            } else {
+                this.mViewHolder.checkParticipate.setChecked(false);
+            }
+        }
+    }
+
+    private static class ViewHolder {
         CheckBox checkParticipate;
     }
 }
---
+++ FestaFimDeAno/app/src/main/java/santos/williankaminski/festafimdeano/views/MainActivity.java
@@ -0,0 +1,61 @@
+package santos.williankaminski.festafimdeano.views;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import santos.williankaminski.festafimdeano.R;
+import santos.williankaminski.festafimdeano.constants.FimDeAnoConstants;
+import santos.williankaminski.festafimdeano.util.SecurityPreferences;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder = new ViewHolder();
+    private SecurityPreferences mSharedPreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.mViewHolder.textToday = (TextView) findViewById(R.id.text_today);
+        this.mViewHolder.daysLeft = (TextView) findViewById(R.id.text_days_left);
+        this.mViewHolder.confirm = (Button) findViewById(R.id.button_confirm);
+
+        this.mViewHolder.confirm.setOnClickListener(this);
+
+        this.mSharedPreferences = new SecurityPreferences(this);
+        this.verifyPreferences();
+    }
+
+    private void verifyPreferences() {
+        String preference = this.mSharedPreferences.getStorageString(FimDeAnoConstants.preferences);
+        if (preference.isEmpty()) {
+            this.mViewHolder.confirm.setText(R.string.nao_confirmado);
+        } else if (preference.equals(FimDeAnoConstants.CONFIRMAED_WILL_GO)) {
+            this.mViewHolder.confirm.setText(R.string.sim);
+        } else {
+            this.mViewHolder.confirm.setText(R.string.nao);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.button_confirm) {
+            String preference = this.mSharedPreferences.getStorageString(FimDeAnoConstants.preferences);
+            Intent intent = new Intent(this, DetailsActivity.class);
+            intent.putExtra(FimDeAnoConstants.preferences, preference);
+            startActivity(intent);
+        }
+    }
+
+    private static class ViewHolder {
+        TextView textToday;
+        TextView daysLeft;
+        Button confirm;
+    }
+}
