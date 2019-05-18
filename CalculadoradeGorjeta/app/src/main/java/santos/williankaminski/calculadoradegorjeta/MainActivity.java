42c8e9066eb17b8314c81cc37b73429451ba6732
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-05-18 16:12:08 -0300

    Alterando configurações da interface e instaciando os recursos na classe Main.

--- CalculadoradeGorjeta/app/src/main/java/santos/williankaminski/calculadoradegorjeta/MainActivity.java
+++ CalculadoradeGorjeta/app/src/main/java/santos/williankaminski/calculadoradegorjeta/MainActivity.java
@@ -2,12 +2,28 @@ package santos.williankaminski.calculadoradegorjeta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText valor;
+    private TextView porcetagem;
+    private SeekBar seekBar;
+    private TextView valorGorjeta;
+    private TextView valorFinal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        valor = findViewById(R.id.editTextValor);
+        porcetagem = findViewById(R.id.textViewPorcentagem);
+        seekBar = findViewById(R.id.seekBar);
+        valorGorjeta = findViewById(R.id.textViewGorjeta);
+        valorFinal = findViewById(R.id.textViewValorTotal);
+
     }
 }
