49a1bd5c93741c93fcc2e7a672e703d80a1e4e80
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-05-18 22:14:40 -0300

    Criando um Array de itens dos quais serão exibidos após a configuração do adatador de lista (Adapter).

--- ListView/app/src/main/java/santos/williankaminski/listview/MainActivity.java
+++ ListView/app/src/main/java/santos/williankaminski/listview/MainActivity.java
@@ -2,12 +2,36 @@ package santos.williankaminski.listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listLocais;
+    private String[] itens = {
+            "Angra dos Reis", "Caldas Novas",
+            "Campos do Jordão", "Costa do Sauipe",
+            "Ihéus", "Porto Seguro", "Tiradentes",
+            "Praga", "Santiago", "Zurique", "Caribe",
+            "Buenos Aires", "Budapeste", "Cancún", "Aruba"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listLocais = findViewById(R.id.listLocais);
+
+        //Criar adaptador para a lista
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1, // --> Layout padrão do Android, padrão este que pode ser customizado
+                android.R.id.text1,
+                itens
+        );
+
+        //Adicionar adaptador para a lista
+        listLocais.setAdapter(adaptador);
     }
 }
