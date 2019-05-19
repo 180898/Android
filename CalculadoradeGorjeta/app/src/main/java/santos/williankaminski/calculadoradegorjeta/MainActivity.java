d489b7cae460304e22f7bbf1b2358793376d7528
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-05-18 21:56:37 -0300

    Criando o Projeto e utilizando o componente ListView.

--- CalculadoradeGorjeta/app/src/main/java/santos/williankaminski/calculadoradegorjeta/MainActivity.java
+++ CalculadoradeGorjeta/app/src/main/java/santos/williankaminski/calculadoradegorjeta/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
