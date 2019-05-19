8b5aeec89a2e05af90eb895b04517970dc47370a
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-05-19 20:59:36 -0300

    Configurando o FrameLayout e simplificando a remoção da sombra da ActionBar.

--- Fragments/app/src/main/java/santos/williankaminski/fragments/MainActivity.java
+++ Fragments/app/src/main/java/santos/williankaminski/fragments/MainActivity.java
@@ -1,20 +1,16 @@
 package santos.williankaminski.fragments;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActionBar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Remover a sombra inferior da ToolBar
-        toolbar=getSupportActionBar();
-        toolbar.setElevation(0);
+        //Remover a sombra da ActionBar
+        getSupportActionBar().setElevation(0);
     }
 }
