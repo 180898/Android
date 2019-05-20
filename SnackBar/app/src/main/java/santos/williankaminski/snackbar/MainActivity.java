5baeebf45f750b896a4883a6b8d77f89c585f6bc
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-05-19 22:20:38 -0300

    Trabalhando com o evento de clique no SnackBar.

--- SnackBar/app/src/main/java/santos/williankaminski/snackbar/MainActivity.java
+++ SnackBar/app/src/main/java/santos/williankaminski/snackbar/MainActivity.java
@@ -4,8 +4,10 @@ package santos.williankaminski.snackbar;
  * Created by Willian Kaminski on 19/05/19.
  */
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +20,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         buttonAbrir = findViewById(R.id.buttonAbrir);
+
+        buttonAbrir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view,"Botão pressionado", Snackbar.LENGTH_LONG)
+                        .setAction("Confirmar", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                buttonAbrir.setText("Botão Alterado");
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.colorPrimary))
+                        .show();
+            }
+        });
     }
 }
