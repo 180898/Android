f604a0612ab34b64b82abfbda7164fc84bca3a65
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-06-05 21:41:10 -0300

    Aplicando os métodos para a manipulação da mídia de som.

--- MidiaPlayer/app/src/main/java/santos/williankaminski/midiaplayer/MainActivity.java
+++ MidiaPlayer/app/src/main/java/santos/williankaminski/midiaplayer/MainActivity.java
@@ -1,13 +1,43 @@
 package santos.williankaminski.midiaplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+    }
+
+    public void executarSom(View v){
+        if(mediaPlayer != null){
+            mediaPlayer.start();
+        }
+    }
+
+    public void pausarMusica(View v){
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+        }else{
+            Toast.makeText(this,"Não há nenhuma música em execução!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void pararMusica(View v){
+        if(mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+        }else{
+            Toast.makeText(this,"Não há nenhuma música em execução!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
