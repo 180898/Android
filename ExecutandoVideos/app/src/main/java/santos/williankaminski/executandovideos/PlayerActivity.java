0f5983973050cee5b5924afab30073dc007ae574
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-06-13 23:32:48 -0300

    Configurando a execução do video em Fullscreeen.

--- ExecutandoVideos/app/src/main/java/santos/williankaminski/executandovideos/PlayerActivity.java
+++ ExecutandoVideos/app/src/main/java/santos/williankaminski/executandovideos/PlayerActivity.java
@@ -2,6 +2,7 @@ package santos.williankaminski.executandovideos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -15,6 +16,14 @@ public class PlayerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_player);
 
         videoView = findViewById(R.id.videoView);
+
+        //Esconder a statusBar e barra de navegação
+        View decorView = getWindow().getDecorView();
+        int UIOpcoes = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(UIOpcoes);
+
+        //Esconder a Actionbar
+        getSupportActionBar().hide();
 
         //Executar o vídeo
         videoView.setMediaController(new MediaController(this));
