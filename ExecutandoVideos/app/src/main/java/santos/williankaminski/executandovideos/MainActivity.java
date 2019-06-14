200f42b3f15169bef600f0d834bd271fd83e2c9d
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-06-13 23:25:26 -0300

    Desenvolvendo a interface e juntamente o método para a execução do video.

--- ExecutandoVideos/app/src/main/java/santos/williankaminski/executandovideos/MainActivity.java
+++ ExecutandoVideos/app/src/main/java/santos/williankaminski/executandovideos/MainActivity.java
@@ -1,7 +1,10 @@
 package santos.williankaminski.executandovideos;
 
+import android.content.Intent;
+import android.sax.StartElementListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void abrirVideo(View v){
+        startActivity(new Intent(this, PlayerActivity.class));
     }
 }
