93b5910b3681dacf79053b2b581065e234889a72
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-06-15 12:38:36 -0300

    Desenvolvendo o Layout da aplicação.

---
+++ Abas/app/src/main/java/santos/williankaminski/abas/MainActivity.java
@@ -0,0 +1,30 @@
+package santos.williankaminski.abas;
+
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Adapter;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private SmartTabLayout smartTabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Configurar Abas
+        FragmentPagerAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                .create()
+        );
+    }
+}
