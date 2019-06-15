c4888fbdc91f5c29d97dc096f75845a21ead5590
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-06-15 15:57:32 -0300

    Configurando a execução dos som de tradução para os animais.

--- AprendaIngles/app/src/main/java/santos/williankaminski/aprendaingles/Fragment/AnimaisFragment.java
+++ AprendaIngles/app/src/main/java/santos/williankaminski/aprendaingles/Fragment/AnimaisFragment.java
@@ -1,19 +1,31 @@
 package santos.williankaminski.aprendaingles.Fragment;
 
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import santos.williankaminski.aprendaingles.Activity.MainActivity;
 import santos.williankaminski.aprendaingles.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AnimaisFragment extends Fragment {
+public class AnimaisFragment extends Fragment implements View.OnClickListener{
 
+    private ImageButton buttonCao;
+    private ImageButton buttonGato;
+    private ImageButton buttonLeao;
+    private ImageButton buttonMacaco;
+    private ImageButton buttonOvelha;
+    private ImageButton buttonVaca;
+
+    private MediaPlayer mediaPlayer;
 
     public AnimaisFragment() {
         // Required empty public constructor
@@ -24,7 +36,61 @@ public class AnimaisFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_animais, container, false);
+        View view =  inflater.inflate(R.layout.fragment_animais, container, false);
+
+        buttonCao = view.findViewById(R.id.buttonCao);
+        buttonGato = view.findViewById(R.id.buttonGato);
+        buttonLeao = view.findViewById(R.id.buttonLeao);
+        buttonMacaco = view.findViewById(R.id.buttonMacaco);
+        buttonOvelha = view.findViewById(R.id.buttonOvelha);
+        buttonVaca = view.findViewById(R.id.buttonVaca);
+
+        buttonCao.setOnClickListener(this);
+        buttonGato.setOnClickListener(this);
+        buttonLeao.setOnClickListener(this);
+        buttonMacaco.setOnClickListener(this);
+        buttonOvelha.setOnClickListener(this);
+        buttonVaca.setOnClickListener(this);
+
+        return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonCao:
+                playSound(R.raw.dog);
+                break;
+            case R.id.buttonGato:
+                playSound(R.raw.cat);
+                break;
+            case R.id.buttonLeao:
+                playSound(R.raw.lion);
+                break;
+            case R.id.buttonMacaco:
+                playSound(R.raw.monkey);
+                break;
+            case R.id.buttonOvelha:
+                playSound(R.raw.sheep);
+                break;
+            case R.id.buttonVaca:
+                playSound(R.raw.cow);
+                break;
+
+        }
+    }
+
+    public void playSound(int sound){
+        mediaPlayer = MediaPlayer.create(getActivity(), sound);
+        if(mediaPlayer != null){
+            mediaPlayer.start();
+
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    mediaPlayer.release();
+                }
+            });
+        }
+    }
 }
