09451d4d4f9998e3851d4f1cb23914308af9a917
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-06-16 17:12:08 -0300

    Trabalhando com o evento de clique para cada item do menu.

--- Menus/app/src/main/java/santos/williankaminski/menus/MainActivity.java
+++ Menus/app/src/main/java/santos/williankaminski/menus/MainActivity.java
@@ -3,6 +3,8 @@ package santos.williankaminski.menus;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_principal, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.item_salvar:
+                Toast.makeText(getApplicationContext(), "Salvar", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.item_editar:
+                Toast.makeText(getApplicationContext(), "Editar", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.item_configurar:
+                Toast.makeText(getApplicationContext(), "Configurar", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
