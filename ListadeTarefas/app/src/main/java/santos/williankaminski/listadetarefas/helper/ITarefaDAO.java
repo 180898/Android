c219650a8d37086b6da109b6d70be43899d76e4f
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-06-20 17:24:25 -0300

    Criando a interface com os métodos que serão obrigatorios.

---
+++ ListadeTarefas/app/src/main/java/santos/williankaminski/listadetarefas/helper/ITarefaDAO.java
@@ -0,0 +1,13 @@
+package santos.williankaminski.listadetarefas.helper;
+
+import java.util.List;
+
+import santos.williankaminski.listadetarefas.Model.Tarefa;
+
+public interface ITarefaDAO {
+
+    public boolean salvar(Tarefa tarefa);
+    public boolean atualizar(Tarefa tarefaa);
+    public boolean deletar(Tarefa tarefa);
+    public List<Tarefa> listar(Tarefa tarefa);
+}
