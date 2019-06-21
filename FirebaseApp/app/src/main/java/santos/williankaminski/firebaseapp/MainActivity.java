28ed4a9abf0703fc713920fe4c6bb38072ce0268
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-06-20 21:30:57 -0300

    Inserindo dados no Firebase.

--- FirebaseApp/app/src/main/java/santos/williankaminski/firebaseapp/MainActivity.java
+++ FirebaseApp/app/src/main/java/santos/williankaminski/firebaseapp/MainActivity.java
@@ -4,12 +4,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /**
+         * Criando um filho e adicionando um valor para
+         * o mesmo no momento da execução do app.
+         */
+        referencia.child("pontos").setValue("200");
+
+
+        /**
+         * Criando novos registros na base de usuários
+         */
+        referencia.child("usuarios").child("001").child("nome").setValue("Willian");
+        referencia.child("usuarios").child("001").child("sobrenome").setValue("Kaminski");
+
+        referencia.child("usuarios").child("002").child("nome").setValue("Amanda");
+        referencia.child("usuarios").child("002").child("sobrenome").setValue("Decarli");
+
+        referencia.child("usuarios").child("003").child("nome").setValue("Bruna");
+        referencia.child("usuarios").child("003").child("sobrenome").setValue("Forsh");
     }
 }
