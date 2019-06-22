97af78151607556629a55e9991cd70dad44ece11
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-06-22 11:50:57 -0300

    Consultando os registros do nó 'usuarios' no Firebase e exibindo os resultados no Logcat.

--- FirebaseApp/app/src/main/java/santos/williankaminski/firebaseapp/MainActivity.java
+++ FirebaseApp/app/src/main/java/santos/williankaminski/firebaseapp/MainActivity.java
@@ -1,10 +1,15 @@
 package santos.williankaminski.firebaseapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
         /**
          * Guardando um objeto na base de dados
          */
+
+        /*
         DatabaseReference usuarios = referencia.child("usuarios");
 
         Usuario usuario = new Usuario();
@@ -49,6 +56,25 @@ public class MainActivity extends AppCompatActivity {
         usuario.setIdades(18);
 
         usuarios.child("001").setValue(usuario);
+        */
+
+        /**
+         * Recuperando os dados do Firebase
+         */
+        DatabaseReference usuarios = referencia.child("usuarios");
+
+        usuarios.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.i("FIREBASE", dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
 
     }
 }
