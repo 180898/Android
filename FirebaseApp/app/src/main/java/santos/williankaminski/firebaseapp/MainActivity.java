ebba7bf385140b876969226a9d8059853c423a9c
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-06-22 12:24:06 -0300

    Realizando o cadastro de usuários utilizando um email e senha.

--- FirebaseApp/app/src/main/java/santos/williankaminski/firebaseapp/MainActivity.java
+++ FirebaseApp/app/src/main/java/santos/williankaminski/firebaseapp/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -14,6 +18,7 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
 
     private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
+    private FirebaseAuth usuario = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,7 +83,21 @@ public class MainActivity extends AppCompatActivity {
         });
         */
 
-
+        /**
+         * Cadastrando usuário
+         */
+        usuario.createUserWithEmailAndPassword(
+                "","").
+                addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Log.i("CreateUser","Sucesso ao cadastrar usuário!");
+                        }else{
+                            Log.i("CreateUser","Erro ao cadastrar usuário!");
+                        }
+                    }
+                });
 
     }
 }
