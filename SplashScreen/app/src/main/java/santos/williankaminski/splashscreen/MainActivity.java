8bf06796a68c08dcc4a508cfb08dcc530b6a6a38
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-07-06 16:08:31 -0300

    Configurando o Splash Screen.

--- SplashScreen/app/src/main/java/santos/williankaminski/splashscreen/MainActivity.java
+++ SplashScreen/app/src/main/java/santos/williankaminski/splashscreen/MainActivity.java
@@ -2,13 +2,33 @@ package santos.williankaminski.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN_TIME_OUT = 8000;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
