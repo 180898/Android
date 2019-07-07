72e973b0017f794980c53a2d8e1d93eeeee87a24
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-07-07 13:03:05 -0300

    Configurando o Slider.

--- Slider10/app/src/main/java/santos/williankaminski/slider10/MainActivity.java
+++ Slider10/app/src/main/java/santos/williankaminski/slider10/MainActivity.java
@@ -1,14 +1,36 @@
 package santos.williankaminski.slider10;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.WindowManager;
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntro2;
 
-public class MainActivity extends AppCompatActivity {
+import santos.williankaminski.slider10.helper.SampleSlide;
+
+public class MainActivity extends AppIntro {
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        /*setContentView(R.layout.activity_main);*/
+
+        addSlide(SampleSlide.newInstance(R.layout.intro_1));
+        addSlide(SampleSlide.newInstance(R.layout.intro_2));
+
+        // Altera a cor da barra inferior
+        //setBarColor(Color.parseColor("#3F51B5"));
+
+        // Altera a cor da linha de separação da barra inferior
+        setSeparatorColor(Color.TRANSPARENT);
+
+        setSkipText("teste");
     }
 }
