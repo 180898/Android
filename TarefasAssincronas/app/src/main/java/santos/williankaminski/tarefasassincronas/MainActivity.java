39b525dc8b241cfa5da2657b0eea391f3d14f08c
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-07-10 23:37:44 -0300

    Trabalhando com tarefas assincronas

--- TarefasAssincronas/app/src/main/java/santos/williankaminski/tarefasassincronas/MainActivity.java
+++ TarefasAssincronas/app/src/main/java/santos/williankaminski/tarefasassincronas/MainActivity.java
@@ -2,13 +2,73 @@ package santos.williankaminski.tarefasassincronas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressBar = findViewById(R.id.progressBar);
+    }
+
+    public void iniciarAsyncTask(View v){
+        MyAsyncTask myAsyncTask = new MyAsyncTask();
+        myAsyncTask.execute(10);
+    }
+
+    /**
+     * 1 -> Parâmetro a ser passado para a classe
+     * 2 -> Tipo de valor que será utilizado para o progresso da tarefa
+     * 3 -> Retorno após tarefa finalizada
+     */
+    class MyAsyncTask extends AsyncTask<Integer, Integer, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(Integer... integers) {
+            int num = integers[0];
+            for(int i = 0; i < num; i++){
+
+                publishProgress(i);
+
+                try{
+                    Thread.sleep(1000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+
+
+            }
+            return "Finalizado";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            progressBar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(
+                    getApplicationContext(),
+                    s, Toast.LENGTH_LONG).show();
+            progressBar.setProgress(0);
+            progressBar.setVisibility(View.INVISIBLE);
+        }
     }
 }
